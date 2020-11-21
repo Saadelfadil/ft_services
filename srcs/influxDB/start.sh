f80f46f2b77a26da86083e2392c83559228f7eb9
Saad El fadil <sel-fadi@e2r7p2.1337.ma>
2020-11-21 19:15:36 +0100

    .

--- srcs/influxDB/start.sh
+++ srcs/influxDB/start.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-
-influxd run
+!/bin/sh
+influxd run &
+influx -execute "create database telegraf"
+influx -execute "create user telegraf with password 'telegraf' with all privileges"
+exec /usr/bin/telegraf
+tail -F /dev/null
--- srcs/telegraf/start.sh
+++ srcs/telegraf/start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+infulx -execute "create database telegraf"
+influx -execute "create user telegraf with password 'telegraf' with all privileges"
 exec /usr/bin/telegraf $@
