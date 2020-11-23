5353d9b09f2d5bb97952ae138296c99bd5cf8eec
Saad El fadil <sel-fadi@e2r7p2.1337.ma>
2020-11-23 12:37:02 +0100

    .

--- srcs/phpmyadmin/pma.sh
+++ srcs/phpmyadmin/pma.sh
@@ -21,6 +21,7 @@ wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-l
 tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+mv ../config.inc.php /www/phpmyadmin/config.sample.inc.php
 rc
 rc-service php-fpm7 start
 rc-service nginx start
